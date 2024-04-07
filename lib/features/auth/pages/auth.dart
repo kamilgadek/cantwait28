bca89ec7c6e449315390d002081c83dec79ed91e
Kamil Gadek <kamil.gadek124@gmail.com>
2024-04-07 20:13:21 +0200

    Add flutter fire ui

--- lib/features/auth/pages/auth.dart
+++ lib/features/auth/pages/auth.dart
@@ -1,29 +1,28 @@
 import 'package:cantwait28/features/home/pages/home_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
-
 class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
+  const AuthGate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-  stream: FirebaseAuth.instance.authStateChanges(),
-  // If the user is already signed-in, use it as initial data
-  initialData: FirebaseAuth.instance.currentUser,
-  builder: (context, snapshot) {
-    // User is not signed in
-    if (!snapshot.hasData) {
-      return const SignInScreen(
-        
-      );
-    }
+    return StreamBuilder<auth.User?>(
+      stream: auth.FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
+          );
+        }
 
-    // Render your application if authenticated
-    return const HomePage();
-  },
-);
+        // Render your application if authenticated
+        return const HomePage();
+      },
+    );
   }
 }
